9933c889042f639cfbc7d20660276c6f0ac38ff5
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2026-03-05 14:02:26 +0300

    Complete eeg encoder

--- graph_attention/models/__init__.py
+++ graph_attention/models/__init__.py
@@ -1,4 +1,4 @@
 from .layers import *
-from .backbone import Transformer, TransformerBlock
+from .transformer import Transformer, TransformerBlock
 from .simple_vit import SimpleViT
 from .vit import ViT
